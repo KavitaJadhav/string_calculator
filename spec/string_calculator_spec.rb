368c8e526f08bd7f04b81d0efb5b94130a68046c
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2021-07-22 04:12:42 +0530

    Adds failing spec to return sum of numbers seperated by comma.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,5 +18,13 @@ RSpec.describe StringCalculator, 'model' do
         expect(string_calculator.add('1')).to eq(1)
       end
     end
+
+    context 'when multiple number separated by comma are given as input' do
+      it 'should return sum of given numbers' do
+        string_calculator = StringCalculator.new
+
+        expect(string_calculator.add('1,2')).to eq(3)
+      end
+    end
   end
 end
