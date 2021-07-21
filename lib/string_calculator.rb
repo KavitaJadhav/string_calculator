82cc58ff895f1fae973b19e35ce3c6cc4745e84f
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2021-07-22 05:26:44 +0530

    Refactored add method - converted if_else statement to if

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,14 +2,14 @@ class StringCalculator
   def add numbers
     return 0 if numbers.empty?
 
+    delimiters = [',', "\n"]
+
     if numbers.start_with?('//')
       default_delimeter = numbers.split(/\n/)[0].gsub('//', '')
+      delimiters << default_delimeter
       numbers.gsub!("//#{default_delimeter}\n", '')
-      delimeters = /\n|,|#{default_delimeter}/
-    else
-      delimeters = /\n|,/
     end
-    
-    numbers.split(delimeters).sum(&:to_i)
+
+    numbers.split(Regexp.union(delimiters)).sum(&:to_i)
   end
 end
