ce3818fbbf80b13d73f39d7a6868233dc24123ef
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2021-07-22 03:57:13 +0530

    Adds failing spec to give sum of lank string as 0

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,4 +2,13 @@ require 'string_calculator'
 
 RSpec.describe StringCalculator, 'model' do
 
+  describe '#add' do
+    context 'when empty string is given as input' do
+      it 'should return 0' do
+        string_calculator = StringCalculator.new
+
+        expect(string_calculator.add('')).to eq(0)
+      end
+    end
+  end
 end
