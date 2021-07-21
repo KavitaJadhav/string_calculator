bcc7351e0a2781d88b026762a9a3df8047b589a8
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2021-07-22 05:02:51 +0530

    Making test pass - sum of numbers having delimeter provided as part of input.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,13 @@
 class StringCalculator
   def add numbers
-    numbers.empty? ? 0 : numbers.split(/\n|,/).sum(&:to_i)
+    return 0 if numbers.empty?
+
+    if numbers.start_with?('//')
+      default_delimeter = numbers.split(/\n/)[0].gsub('//', '')
+      numbers.gsub!("//#{default_delimeter}\n", '')
+      numbers.split(/\n|,|#{default_delimeter}/).sum(&:to_i)
+    else
+      numbers.split(/\n|,/).sum(&:to_i)
+    end
   end
 end
