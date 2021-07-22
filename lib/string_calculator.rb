6dd5787a92deda31dfdc610f9c166b1f14cecc47
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2021-07-22 06:45:18 +0530

    Adds logic to ignore values beyond 1000 for sum

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -11,6 +11,8 @@ class StringCalculator
     values = values_from(numbers)
     negative_values = values.select {|value| value < 0}
     raise StandardError.new "negatives not allowed: #{negative_values.join(',')}" unless negative_values.empty?
+
+    values.reject! {|value| value > 1000}
     values.sum
   end
 
