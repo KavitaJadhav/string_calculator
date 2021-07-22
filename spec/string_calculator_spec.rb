292d12b176c9a088559f9d8f5227be7a84586b75
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2021-07-22 06:26:43 +0530

    Creates string_calculator using let method

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,60 +1,47 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator, 'model' do
+  let(:string_calculator) {StringCalculator.new}
 
   describe '#add' do
     context 'when empty string is given as input' do
       it 'should return 0' do
-        string_calculator = StringCalculator.new
-
         expect(string_calculator.add('')).to eq(0)
       end
     end
 
     context 'when single number is given as string input' do
       it 'should return same number' do
-        string_calculator = StringCalculator.new
-
         expect(string_calculator.add('1')).to eq(1)
       end
     end
 
     context 'when multiple number separated by comma are given as input' do
       it 'should return sum of given numbers' do
-        string_calculator = StringCalculator.new
-
         expect(string_calculator.add('1,2')).to eq(3)
       end
     end
 
     context 'when multiple number separated by comma or new_line character are given as input' do
       it 'should return sum of given numbers' do
-        string_calculator = StringCalculator.new
-
         expect(string_calculator.add("1\n2,3")).to eq(6)
       end
     end
 
     context 'when delimeter is given as part of number ' do
       it 'should return sum of given numbers including numbers separated by given delimeter' do
-        string_calculator = StringCalculator.new
-
         expect(string_calculator.add("//;\n1;2")).to eq(3)
       end
     end
 
     context 'when numbers include negative values' do
       it 'should throw an error as negatives not allowed along with negative number' do
-        string_calculator = StringCalculator.new
-
         expect {string_calculator.add("//;\n1;-2\n3")}.to raise_error(StandardError, 'negatives not allowed: -2')
       end
     end
 
     context 'when numbers include multiple negative values' do
       it 'should throw an error as negatives not allowed along with all negative number' do
-        string_calculator = StringCalculator.new
-
         expect {string_calculator.add("//;\n1;-2\n3,-4\n5,-6")}.to raise_error(StandardError, 'negatives not allowed: -2,-4,-6')
       end
     end
